191a934a96108409a635038c74fbc5eea2304911
Pavel Staykov <73100735+Pavalunkata@users.noreply.github.com>
2023-12-23 23:06:46 +0200

    calculating the mass

--- src/main/java/cranedistancecalculator/BidjoKranApplication.java
+++ src/main/java/cranedistancecalculator/BidjoKranApplication.java
@@ -2,13 +2,14 @@ package cranedistancecalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class BidjoKranApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BidjoKranApplication.class, args);
@@ -17,10 +18,18 @@ public class BidjoKranApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
-//
-//	@GetMapping("/")
-//	public String home() {
-//		return "index";
-//	}
+
+	@GetMapping("/distance")
+	public String distance(@RequestParam(name = "distance", required = false) Double distance,
+						   @RequestParam(name = "height", required = false) Double height) {
+		if (distance != null && height != null) {
+			double mass = distanceMassTable.massLimit(distance, height);
+			return "" + mass;
+		} else {
+			return "index";
+		}
+
+
+	}
 
 }
--- src/main/java/cranedistancecalculator/distanceMassTable.java
+++ src/main/java/cranedistancecalculator/distanceMassTable.java
@@ -1,18 +1,18 @@
 package cranedistancecalculator;
 
 public class distanceMassTable {
-    public static double massLimit(double length, double height) {
+    public static double massLimit(double distance, double height) {
         double mass = 0;
 
-        if (length <= 2.5){
+        if (distance <= 2.5){
             if (height <= 9){
                 mass = 30;
             }
-        } else if (length <= 3) {
+        } else if (distance <= 3) {
             if (height <= 9){
                 mass = 22.8;
             }
-        } else if (length <= 4) {
+        } else if (distance <= 4) {
             if (height <= 8){
                 mass = 18.8;
             } else if (height <= 21) {
@@ -20,7 +20,7 @@ public class distanceMassTable {
             } else if (height <= 25) {
                 mass = 12;
             }
-        } else if (length <= 5) {
+        } else if (distance <= 5) {
             if (height <=7){
                 mass = 15.8;
             } else if (height <= 14) {
@@ -32,7 +32,7 @@ public class distanceMassTable {
             } else if (height <= 27) {
                 mass = 10;
             }
-        } else if (length <=6) {
+        } else if (distance <=6) {
             if (height <= 5){
                 mass = 12.5;
             } else if (height <= 14) {
@@ -40,7 +40,7 @@ public class distanceMassTable {
             } else if (height <= 20) {
                 mass = 12.5;
             }
-        } else if (length <= 7) {
+        } else if (distance <= 7) {
             if (height <= 13){
                 mass = 10.9;
             } else if (height <= 20) {
@@ -52,7 +52,7 @@ public class distanceMassTable {
             } else if (height <= 35) {
                 mass = 4.1;
             }
-        } else if (length <= 8) {
+        } else if (distance <= 8) {
             if (height <= 12){
                 mass = 9.1;
             } else if (height <= 21) {
@@ -66,7 +66,7 @@ public class distanceMassTable {
             } else if (height <= 41) {
                 mass = 2.1;
             }
-        } else if (length <=9) {
+        } else if (distance <=9) {
             if (height <= 11){
                 mass = 7.6;
             } else if (height <= 19) {
@@ -76,7 +76,7 @@ public class distanceMassTable {
             } else if (height <= 26) {
                 mass = 6.5;
             }
-        } else if (length <= 10) {
+        } else if (distance <= 10) {
             if (height <= 10){
                 mass = 6.4;
             } else if (height <= 18) {
@@ -90,7 +90,7 @@ public class distanceMassTable {
             } else if (height <= 41) {
                 mass = 1.9;
             }
-        } else if (length <= 12) {
+        } else if (distance <= 12) {
             if (height <= 6){
                 mass = 4.8;
             } else if (height <= 17) {
@@ -104,7 +104,7 @@ public class distanceMassTable {
             } else if (height <= 40) {
                 mass = 1.8;
             }
-        } else if (length <= 14) {
+        } else if (distance <= 14) {
             if (height <= 14){
                 mass = 3.8;
             } else if (height <= 21) {
@@ -116,7 +116,7 @@ public class distanceMassTable {
             } else if (height <= 39) {
                 mass = 1.6;
             }
-        } else if (length <= 16) {
+        } else if (distance <= 16) {
             if (height <= 11){
                 mass = 3;
             } else if (height <= 18) {
@@ -128,7 +128,7 @@ public class distanceMassTable {
             } else if (height <= 38) {
                 mass = 1.5;
             }
-        } else if (length <= 18) {
+        } else if (distance <= 18) {
             if (height <= 6){
                 mass = 2.5;
             } else if (height <= 16) {
@@ -140,7 +140,7 @@ public class distanceMassTable {
             } else if (height <= 37) {
                 mass = 1.4;
             }
-        }else if (length <= 20) {
+        }else if (distance <= 20) {
             if (height <= 12){
                 mass = 2;
             } else if (height <= 16) {
@@ -150,7 +150,7 @@ public class distanceMassTable {
             } else if (height <= 36) {
                 mass = 1.4;
             }
-        } else if (length <= 22) {
+        } else if (distance <= 22) {
             if (height <= 7){
                 mass = 1.7;
             } else if (height <= 12) {
@@ -160,7 +160,7 @@ public class distanceMassTable {
             } else if (height <= 35) {
                 mass = 1.2;
             }
-        } else if (length <= 24) {
+        } else if (distance <= 24) {
             if (height <= 5){
                 mass = 1.3;
             } else if (height <= 24) {
@@ -168,33 +168,33 @@ public class distanceMassTable {
             } else if (height <= 33) {
                 mass = 1.2;
             }
-        } else if (length <= 26) {
+        } else if (distance <= 26) {
             if (height <= 22) {
                 mass = 1.2;
             } else if (height <= 31) {
                 mass = 1.1;
             }
-        } else if (length <= 28) {
+        } else if (distance <= 28) {
             if (height <= 19) {
                 mass = 1;
             } else if (height <= 29) {
                 mass = 1;
             }
-        } else if (length <= 30) {
+        } else if (distance <= 30) {
             if (height <= 14) {
                 mass = 0.9;
             } else if (height <= 27) {
                 mass = 0.8;
             }
-        } else if (length <= 32) {
+        } else if (distance <= 32) {
             if (height <= 24) {
                 mass = 0.7;
             }
-        } else if (length <= 34) {
+        } else if (distance <= 34) {
             if (height <= 21) {
                 mass = 0.6;
             }
-        } else if (length <= 36) {
+        } else if (distance <= 36) {
             if (height <= 16) {
                 mass = 0.5;
             }
