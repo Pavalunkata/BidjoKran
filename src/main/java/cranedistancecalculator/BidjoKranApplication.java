a6b22c525a3c69a717e56aaf590562f8d289e748
Pavel Staykov <73100735+Pavalunkata@users.noreply.github.com>
2023-12-23 19:35:48 +0200

    migrating to maven and organising resources

--- src/main/java/cranedistancecalculator/BidjoKranApplication.java
+++ src/main/java/cranedistancecalculator/BidjoKranApplication.java
@@ -3,6 +3,7 @@ package cranedistancecalculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class BidjoKranApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "home.html";
 	}
 
 }
--- src/main/java/cranedistancecalculator/Main.java
+++ src/main/java/cranedistancecalculator/Main.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the length: ");
-        double length = Double.parseDouble(scanner.nextLine());
-        System.out.print("Enter the height: ");
-        double height = Double.parseDouble(scanner.nextLine());
-
-        double mass = distanceMassTable.massLimit(length, height);
-        System.out.println(mass);
-    }
-}
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter the length: ");
+//        double length = Double.parseDouble(scanner.nextLine());
+//        System.out.print("Enter the height: ");
+//        double height = Double.parseDouble(scanner.nextLine());
+//
+//        double mass = distanceMassTable.massLimit(length, height);
+//        System.out.println(mass);
+//    }
+//}
